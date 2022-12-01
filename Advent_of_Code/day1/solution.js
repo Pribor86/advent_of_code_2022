61f1b6e3b09b7c3ad7880ad53e73414c7d9366b1
Pribor86 <pribor86@gmail.com>
2022-12-01 21:28:01 +0200

    Refactor

--- Advent_of_Code/day1/solution.js
+++ Advent_of_Code/day1/solution.js
@@ -3,22 +3,21 @@ const path = require('path');
 
 const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
 
-const inputArray = input.split(/\n\s*\n/);
-const inputArray2 = inputArray.map((item) => item.replace(/\n/g, ' '));
+const inputArray = input.split(/\n\s*\n/).map((item) => item.replace(/\n/g, ' '));
+
 let max = 0;
 let arr = [];
-for (let i = 0; i < inputArray2.length; i++) {
-    const sum = inputArray2[i].split(' ').map(Number).reduce((a, b) => a + b);
-    // 3 top sum
+
+for (let i = 0; i < inputArray.length; i++) {
+    const sum = inputArray[i].split(' ').map(Number).reduce((a, b) => a + b);
     arr.push(sum);
-
-
     if (sum > max) {
         max = sum;
     }
 }
-//sort array and get 3 top sum
+
 arr.sort((a, b) => b - a);
+
 console.log("TOP3 sum", arr[0] + arr[1] + arr[2]);
 console.log("MAX", max);
 
