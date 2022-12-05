2c5b0a460c636e9ba5b46845934fa9b0c4f7a6b5
Pribor86 <pribor86@gmail.com>
2022-12-05 23:39:36 +0200

    Day5 solution second part

--- Advent_of_Code/day5/solution.js
+++ Advent_of_Code/day5/solution.js
@@ -12,7 +12,14 @@ let crateArray = inputArray[0]
 
 let actionArray = inputArray[1]
 
+let actionArrayLines = actionArray.split('\n')
+
+let numbers = actionArrayLines.map((line) => line.match(/\d+/g))
+
+numbers.pop()
+
 console.log(inputArray[0])
+
 let indexes = crateArray.pop();
 let column = {}
 
@@ -27,12 +34,9 @@ for (let line of crateArray) {
     }
 }
 
-let actionArrayLines = actionArray.split('\n')
 
-let numbers = actionArrayLines.map((line) => line.match(/\d+/g))
 
-numbers.pop()
-
+// solution for 1 part
 for (let line of numbers) {
     for (let i = 0; i < line.length; i++) {
         line[i] = parseInt(line[i])
@@ -42,15 +46,42 @@ for (let line of numbers) {
         column[line[1]].pop()
     }
 }
-
-let answer = '';
+let answer1 = '';
 
 for ( let line of Object.entries(column)) {
-    answer += line[line.length - 1][line[line.length - 1].length - 1]
+    answer1 += line[line.length - 1][line[line.length - 1].length - 1]
 }
 
 
-console.log(column)
-console.log(answer)
+// solution for 2 part
+let column2 = {}
 
+for (let line of crateArray) {
+    for (let i = 0; i < line.length; i++) {
+        if (line[i] !== ' ') {
+            if (!column2[indexes[i]]) {
+                column2[indexes[i]] = []
+            }
+            column2[indexes[i]].unshift(line[i])
+        }
+    }
+}
 
+for (let line of numbers) {
+    for (let i = 0; i < line.length; i++) {
+        line[i] = parseInt(line[i])
+    }
+    for (let j = 0; j < line[0]; j++) {
+        column2[line[2]].push(column2[line[1]][column2[line[1]].length - (line[0] - j)])
+        column2[line[1]].splice(column2[line[1]].length - (line[0] - j), 1)
+    }
+}
+
+let answer2 = '';
+
+for ( let line of Object.entries(column2)) {
+    answer2 += line[line.length - 1][line[line.length - 1].length - 1]
+}
+
+console.log(answer1)
+console.log(answer2)
